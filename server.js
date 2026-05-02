07003fbf8bfc3693eb1727ffb233d8ab4755ba33
Dipen <dipendraboyaju222@gmail.com>
2026-05-02 13:29:38 +0545

    updates

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "https://study-buddy-nine-sable.vercel.app/",
+    origin: "https://study-buddy-nine-sable.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
